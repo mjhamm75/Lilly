563a4ab628d5def577e0e888404904f1d89d3761
Jason Hamm <jhamm.business@gmail.com>
2013-07-03 07:33:01 -0400

    Test age and fullname

--- spec/models/scout_spec.rb
+++ spec/models/scout_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Scout do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:scout).should be_valid
+  end
+
+  it "will give the scouts age" do
+    scout = FactoryGirl.create(:scout)
+    scout.age.should eql 15
+  end
+
+  it "will give the full name of the scout" do
+    scout = FactoryGirl.create(:scout)
+    scout.full_name.should eql "Tristan McGraw"
+  end
 end
