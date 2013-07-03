fe211885bedbeea29aaae56ddd29888e54697e66
Jason Hamm <jhamm.business@gmail.com>
2013-07-03 07:33:21 -0400

    View scout in list

--- features/step_definitions/scout_steps.rb
+++ features/step_definitions/scout_steps.rb
@@ -4,18 +4,19 @@ Given(/^I am on the scout list page$/) do
   visit scouts_path
 end
 
-Given(/^I create a scout named Tristan McGraw$/) do
-  pending # express the regexp above with the code you wish you had
+Given /^I create a scout named (.*) (.*)$/ do |first_name, last_name|
+  @scout = Scout.create!(:first_name => first_name, :last_name => last_name)
 end
 
-Given(/^his birthday is (\d+) years ago$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^his birthday is (\d+) years ago$/) do |birthday|
+  @scout.update_attribute(:birthdate, 15.years.ago)
 end
 
 Then(/^I should see him in the list$/) do
-  pending # express the regexp above with the code you wish you had
+  visit scouts_path
+  find('td', text: @scout.first_name)
 end
 
-Then(/^his age should be (\d+)$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^his age should be ([0-9]+)$/ do |age|
+  @scout.age.should eql age.to_i
 end
