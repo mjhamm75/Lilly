e6d13278b2776b281ad38506e4aa4bb9144b2c84
Jason Hamm <jhamm.business@gmail.com>
2013-07-03 07:34:02 -0400

    Add age/full name methods

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -1,2 +1,9 @@
 class Scout < ActiveRecord::Base
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  def age
+    now = Time.now.utc.to_date
+    now.year - self.birthdate.year - (self.birthdate.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
 end
