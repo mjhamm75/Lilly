0685757aeac7302e248f7bce70d841ee7977a1db
Jason Hamm <jhamm.business@gmail.com>
2013-07-16 06:42:37 -0400

    Merit badge list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716100212) do
+ActiveRecord::Schema.define(version: 20130716101350) do
+
+  create_table "merit_badge_lists", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "merit_badges", force: true do |t|
     t.integer  "scout_id"
