95d966456d789b10e25ec9a73b8f5f82fc353b75
Jason Hamm <jhamm.business@gmail.com>
2013-07-17 06:55:35 -0400

    Refactor code to use same modal

--- app/assets/javascripts/scouts.js
+++ app/assets/javascripts/scouts.js
@@ -3,11 +3,39 @@ $(document).ready(function() {
 
   $('#add_new_scout').on('click', function() {
     var template = $("#new_scout_modal").html();
-    create_modal(template);
+    create_modal(template, this.id);
   });
 
   $('#complete').on('click', function(e) {
     e.preventDefault();
+    if(this.id === "complete_add_new_scout") {
+      add_new_scout();
+    }
+    if(this.id === "complete_add_merit_badge") {
+      add_new_merit_badge();
+    }
+    $('#modal_container').modal('hide');
+  });
+
+  $("tr[data-link]").click(function() {
+    window.location = $(this).data("link");
+  });
+
+  $("#add_merit_badge").click(function(e) {
+    e.preventDefault();
+    var template = $("#add_merit_badge_modal").html();
+    create_modal(template, this.id);
+    $('.chzn-select').chosen();
+  });
+
+  var create_modal = function(template, name) {
+    $('.modal-header').html($(template).siblings('.modal-header'));
+    $('.modal-body').html($(template).siblings('.modal-body'));
+    $('.modal-footer #complete').attr("id", "complete_" + name);
+    $('#modal_container').modal();
+  };
+
+  var add_new_scout = function() {
     var req = $.ajax({
       url: '/scouts/new',
       data: { first_name: $('#first_name').val(), last_name: $('#last_name').val(), birthday: $("#birthdate").val()},
@@ -19,23 +47,9 @@ $(document).ready(function() {
     };
 
     req.done(success);
-    $('#modal_container').modal('hide');
-  });
-
-  $("tr[data-link]").click(function() {
-    window.location = $(this).data("link");
-  });
-
-  $("#add_merit_badge").click(function(e) {
-    e.preventDefault();
-    var template = $("#add_merit_badge_modal").html();
-    create_modal(template);
-    $('.chzn-select').chosen();
-  });
-
-  var create_modal = function(template) {
-    $('.modal-header').html($(template).siblings('.modal-header'));
-    $('.modal-body').html($(template).siblings('.modal-body'));
-    $('#modal_container').modal();
   };
+
+  var add_new_merit_badge = function() {
+    console.log("here");
+  }
 });
