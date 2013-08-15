e805de1fa754ed3414727ad07f9a9fea7ad2f3fe
Jason Hamm <jhamm.business@gmail.com>
2013-08-15 07:01:38 -0400

    Delete unused files

--- db/migrate/20130812110135_add_has_options_and_number_options_to_advancement_requirements.rb
+++
@@ -1,6 +0,0 @@
-class AddHasOptionsAndNumberOptionsToAdvancementRequirements < ActiveRecord::Migration
-  def change
-    add_column :advancement_requirements, :has_options, :boolean, :default => false
-    add_column :advancement_requirements, :number_options, :integer
-  end
-end
