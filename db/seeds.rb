15712c1ba9f951460322741d7ed8cd6e73eeec94
Jason Hamm <jhamm.business@gmail.com>
2013-09-02 11:08:11 -0400

    Add American Heritage mb

--- db/seeds.rb
+++ db/seeds.rb
@@ -355,14 +355,107 @@ if MeritBadge.find_by_name("American Cultures").requirements.count == 0
     elsif(req.requirement.internal_id == 30)
       req.update_attributes(:label => "e", :ord => 6, :parent => 25)
     elsif(req.requirement.internal_id == 31)
-      req.update_attributes(:label => "2", :ord => 6)
+      req.update_attributes(:label => "2", :ord => 7)
     elsif(req.requirement.internal_id == 32)
-      req.update_attributes(:label => "3", :ord => 6)
+      req.update_attributes(:label => "3", :ord => 8)
     elsif(req.requirement.internal_id == 33)
-      req.update_attributes(:label => "4", :ord => 6)
+      req.update_attributes(:label => "4", :ord => 9)
     elsif(req.requirement.internal_id == 34)
-      req.update_attributes(:label => "5", :ord => 6)
+      req.update_attributes(:label => "5", :ord => 10)
     end
   end
   puts "Updated American Cultures ar's"
 end
+
+if MeritBadge.find_by_name("American Heritage").requirements.count == 0
+  r = Requirement.create([
+                           {requirement:  "Read the Declaration of Independence. Pay close attention to the section that begins with \"We hold these truths to be self-evident\" and ends with \"to provide new Guards for future security.\" Rewrite that section in your own words, making it as easy to understand as possible. Then share your writing with your merit badge counselor and discuss the importance of the Declaration of Independence.", internal_id: 35},
+                           {requirement:  "Do TWO of the following:", internal_id: 36},
+                           {requirement:  "Select two individuals from American history, one a political leader (a president, senator, etc.) and the other a private citizen (a writer, religious leader, etc.). Find out about each person's accomplishments and compare the contributions each has made to America's heritage.", internal_id: 37},
+                           {requirement:  "With your counselor's approval, choose an organization that has promoted some type of positive change in American society. Find out why the organization believed this change was necessary and how it helped to accomplish the change. Discuss how this organization is related to events or situations from America's past.", internal_id: 38},
+                           {requirement:  "With your counselor's approval, interview two veterans of the U.S. military. Find out what their experiences were like. Ask the veterans what they believe they accomplished.", internal_id: 39},
+                           {requirement:  "With your counselor's approval, interview three people in your community of different ages and occupations. Ask these people what America means to them, what they think is special about this country, and what American traditions they feel are important to preserve.", internal_id: 40},
+                           {requirement: "Do the following:", internal_id: 41},
+                           {requirement:  "Select a topic that is currently in the news. Describe to your counselor what is happening. Explain how today's events are related to or affected by the events and values of America's past.", internal_id: 42},
+                           {requirement:  "For each of the following, describe its adoption, tell about any changes since its adoption, and explain how each one continues to influence Americans today: the flag, the Pledge of Allegiance, the seal, the motto, and the national anthem.", internal_id: 43},
+                           {requirement:  "Research your family's history. Find out how various events and situations in American history affected your family. Share what you find with your counselor. Tell why your family came to America.", internal_id: 44},
+                           {requirement:  "Do TWO of the following:", internal_id: 45},
+                           {requirement:  "Explain what is meant by the National Register of Historic Places. Describe how a property becomes eligible for listing. Make a map of your local area, marking the points of historical interest. Tell about any National Register properties in your area. Share the map with your counselor, and describe the historical points you have indicated.", internal_id: 46},
+                           {requirement:  "Research an event of historical importance that took place in or near your area. If possible, visit the place. Tell your counselor about the event and how it affected local history. Describe how the area looked then and what it now looks like.", internal_id: 47},
+                           {requirement:  "Find out when, why, and how your town or neighborhood started, and what ethnic, national, or racial groups played a part. Find out how the area has changed over the past 50 years and try to explain why.", internal_id: 48},
+                           {requirement:  "Take an active part in a program about an event or person in American history. Report to your counselor about the program, the part you took, and the subject.", internal_id: 49},
+                           {requirement:  "Visit a historic trail or walk in your area. After your visit, share with your counselor what you have learned. Discuss the importance of this location and explain why you think it might qualify for National Register listing.", internal_id: 50},
+                           {requirement:  "Do ONE of the following:", internal_id: 51},
+                           {requirement:  "Watch two motion pictures (with the approval and permission of your counselor and parent) that are set in some period of American history. Describe to your counselor how accurate each film is with regard to the historical events depicted and also with regard to the way the characters are portrayed.", internal_id: 52},
+                           {requirement:  "Read a biography (with your counselor's approval) of someone who has made a contribution to America's heritage. Tell some things you admire about this individual and some things you do not admire. Explain why you think this person has made a positive or a negative contribution to America's heritage.", internal_id: 53},
+                           {requirement:  "Listen to recordings of popular songs from various periods of American history. Share five of these songs with your counselor, and describe how each song reflects the way people felt about the period in which it was popular. If a recording is not available, have a copy of the lyrics available.", internal_id: 54},
+                           {requirement:  "Discuss with your counselor the career opportunities in American heritage. Pick one that interests you and explain how to prepare for this career. Discuss what education and training are required for this career.", internal_id: 55}
+  ])
+  MeritBadge.find_by_name("American Heritage").requirements << r
+  puts "American Heritage reqs created"
+
+  mb = MeritBadge.find_by_name("American Heritage").advancement_requirements.each do |req|
+    if(req.requirement.internal_id == 35)
+      req.update_attributes(:label => "1", :ord => 1)
+    elsif(req.requirement.internal_id == 36)
+      req.update_attributes(:label => "2", :ord => 2,  :children => "37#38#39#40", :children_count => 2)
+    elsif(req.requirement.internal_id == 37)
+      req.update_attributes(:label => "a", :ord => 3,  :parent => 36)
+    elsif(req.requirement.internal_id == 38)
+      req.update_attributes(:label => "b", :ord => 4,  :parent => 36)
+    elsif(req.requirement.internal_id == 39)
+      req.update_attributes(:label => "c", :ord => 5,  :parent => 36)
+    elsif(req.requirement.internal_id == 40)
+      req.update_attributes(:label => "d", :ord => 6,  :parent => 36)
+    elsif(req.requirement.internal_id == 41)
+      req.update_attributes(:label => "3", :ord => 7,  :children => "42#43#44", :children_count => 3)
+    elsif(req.requirement.internal_id == 42)
+      req.update_attributes(:label => "a", :ord => 8,  :parent => 41)
+    elsif(req.requirement.internal_id == 43)
+      req.update_attributes(:label => "b", :ord => 9,  :parent => 41)
+    elsif(req.requirement.internal_id == 44)
+      req.update_attributes(:label => "c", :ord => 10,  :parent => 41)
+    elsif(req.requirement.internal_id == 45)
+      req.update_attributes(:label => "4", :ord => 11,  :children => "46#47#48#49#50", :children_count => 2)
+    elsif(req.requirement.internal_id == 46)
+      req.update_attributes(:label => "a", :ord => 12,  :parent => 45)
+    elsif(req.requirement.internal_id == 47)
+      req.update_attributes(:label => "b", :ord => 13,  :parent => 45)
+    elsif(req.requirement.internal_id == 48)
+      req.update_attributes(:label => "c", :ord => 14,  :parent => 45)
+    elsif(req.requirement.internal_id == 49)
+      req.update_attributes(:label => "d", :ord => 15,  :parent => 45)
+    elsif(req.requirement.internal_id == 50)
+      req.update_attributes(:label => "e", :ord => 16,  :parent => 45)
+    elsif(req.requirement.internal_id == 51)
+      req.update_attributes(:label => "5", :ord => 17,  :children => "52#53#54", :children_count => 1)
+    elsif(req.requirement.internal_id == 52)
+      req.update_attributes(:label => "a", :ord => 18,  :parent => 51)
+    elsif(req.requirement.internal_id == 53)
+      req.update_attributes(:label => "b", :ord => 19,  :parent => 51)
+    elsif(req.requirement.internal_id == 54)
+      req.update_attributes(:label => "c", :ord => 20,  :parent => 51)
+    elsif(req.requirement.internal_id == 55)
+      req.update_attributes(:label => "6", :ord => 21)
+    end
+  end
+  puts "Updated American Heritage ar's"
+end
+
+# if MeritBadge.find_by_name("American Cultures").requirements.count == 0
+#   r = Requirement.create([
+#                            {requirement:  "Tell about a contribution made to our country by three different people each from a different racial, ethnic, or religious background.", internal_id: 33},
+#                            {requirement:  "Give a talk to your Scout unit or class at school on how people from different groups have gotten along together. Lead a discussion on what can be done to help various groups understand one another better.", internal_id: 34}
+#   ])
+#   MeritBadge.find_by_name("American Cultures").requirements << r
+#   puts "American Cultures reqs created"
+
+#   mb = MeritBadge.find_by_name("American Cultures").advancement_requirements.each do |req|
+#     if(req.requirement.internal_id == 25)
+#       req.update_attributes(:label => "1", :ord => 1,  :children => "26#27#28#29#30", :children_count => 2)
+#     elsif(req.requirement.internal_id == 34)
+#       req.update_attributes(:label => "5", :ord => 6)
+#     end
+#   end
+#   puts "Updated American Cultures ar's"
+# end
