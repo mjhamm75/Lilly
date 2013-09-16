aa7bce45ce06609b83dea88cc020897216f2c36b
Jason Hamm <jhamm.business@gmail.com>
2013-09-16 05:54:07 -0400

    Remove requirements when mb is deleted

--- app/controllers/advancements_controller.rb
+++ app/controllers/advancements_controller.rb
@@ -7,7 +7,9 @@ class AdvancementsController < ApplicationController
   def destroy
     @scout = set_scout
     @mb = MeritBadge.find_by_id(params[:id])
+    @requirements = MeritBadge.find_by_id(params[:id]).requirements
     @scout.advancements.delete(@mb)
+    @scout.requirements.delete(@requirements)
     respond_to do |format|
       format.html { redirect_to edit_scout_path(Scout.find_by_id(params[:scout_id])) }
       format.json { head :no_content }
