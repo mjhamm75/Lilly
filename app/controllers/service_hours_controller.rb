be0c505882d939333192f9e4c09e90e37b497f40
Jason Hamm <jhamm.business@gmail.com>
2013-11-07 06:31:55 -0500

    Create helper file and access from controller

--- app/controllers/service_hours_controller.rb
+++ app/controllers/service_hours_controller.rb
@@ -1,4 +1,5 @@
 class ServiceHoursController < ApplicationController
+  include ServiceHoursHelper
   before_action :set_scout, only: [:create]
 
   def create
@@ -7,6 +8,7 @@ class ServiceHoursController < ApplicationController
     mins_worked = mins.to_i + (hours.to_i * 60)
     sh = ServiceHour.create!(:title => params[:title], :place => params[:place], :minutes => mins_worked, :date_of_service => params[:date])
     @scout.service_hours << sh
+    update_service_hours(scout)
 
     respond_to do |format|
       format.json {
---
+++ app/helpers/service_hours_helper.rb
@@ -0,0 +1,6 @@
+module ServiceHoursHelper
+  def update_service_hours(hours)
+    puts "updating hours"
+    puts hours
+  end
+end
