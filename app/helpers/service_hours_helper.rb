afccc0178198ca37efef1df63fb00624e08be6bf
Jason Hamm <jhamm.business@gmail.com>
2013-11-25 19:50:43 -0500

    Find all ranks to check service hours

--- app/helpers/service_hours_helper.rb
+++ app/helpers/service_hours_helper.rb
@@ -1,6 +1,7 @@
 module ServiceHoursHelper
   def update_service_hours(scout)
     totalTimeServedInMinutes = scout.service_hours.sum('minutes')
+    advancements = scout.advancments.ranks
     puts totalTimeServedInMinutes
   end
 end
