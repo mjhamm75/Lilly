4bd3093c7ec6b784b6f581ca169fccdf2c8da675
Jason Hamm <jhamm.business@gmail.com>
2013-11-26 06:59:24 -0500

    helper methods to update service hours in ranks

--- app/helpers/service_hours_helper.rb
+++ app/helpers/service_hours_helper.rb
@@ -1,7 +1,22 @@
 module ServiceHoursHelper
   def update_service_hours(scout)
     totalTimeServedInMinutes = scout.service_hours.sum('minutes')
-    advancements = scout.advancments.ranks
-    puts totalTimeServedInMinutes
+    ranks = scout.advancements.ranks
+    ranks.each do |rank|
+      totalTimeServedInMinutes = update_rank_service_hours(scout, rank, totalTimeServedInMinutes)
+    end
+  end
+
+  def update_rank_service_hours(scout, rank, totalTimeServedInMinutes)
+    minutesNeededForRankRequirement = rank.advancement_requirements.where('service_minutes is not null')
+    minutesNeededForRankRequirement.each do |mn|
+      req = scout.scout_requirements.where(:requirement_id => mn.requirement_id).first
+      if(req.completed_date === nil)
+        puts "None"
+      else
+        puts "FULL"
+      end
+    end
+    totalTimeServedInMinutes
   end
 end
