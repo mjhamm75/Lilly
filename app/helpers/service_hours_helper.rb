3f6be7fc2350a967bbbc32d2df44c8876d2e8a72
Jason Hamm <jhamm.business@gmail.com>
2013-11-27 06:32:31 -0500

    update service hours for ranks by just updating service hours

--- app/helpers/service_hours_helper.rb
+++ app/helpers/service_hours_helper.rb
@@ -8,13 +8,18 @@ module ServiceHoursHelper
   end
 
   def update_rank_service_hours(scout, rank, totalTimeServedInMinutes)
-    minutesNeededForRankRequirement = rank.advancement_requirements.where('service_minutes is not null')
-    minutesNeededForRankRequirement.each do |mn|
-      req = scout.scout_requirements.where(:requirement_id => mn.requirement_id).first
-      if(req.completed_date === nil)
-        puts "None"
+    reqsForService = rank.advancement_requirements.where('service_minutes is not null')
+    if(reqsForService.length > 0)
+      reqForService = reqsForService.first
+      reqId = reqForService.requirement_id
+      sr = scout.scout_requirements.where(:requirement_id => reqId).first
+      if(sr.completed_date == nil)
+        if(totalTimeServedInMinutes > reqForService.service_minutes)
+          sr.update_attributes(:completed_date => Date.today)
+          totalTimeServedInMinutes = totalTimeServedInMinutes - reqForService.service_minutes
+        end
       else
-        puts "FULL"
+        totalTimeServedInMinutes = totalTimeServedInMinutes > reqForService.service_minutes  ? totalTimeServedInMinutes - reqForService.service_minutes : 0
       end
     end
     totalTimeServedInMinutes
