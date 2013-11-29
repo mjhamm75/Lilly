6ea34f3c998cff8ba6f7f347cce8f275f664511e
Jason Hamm <jhamm.business@gmail.com>
2013-11-29 10:29:22 -0500

    Helper test now working

--- spec/helpers/service_hours_helper_spec.rb
+++ spec/helpers/service_hours_helper_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+
+describe "ServiceHoursHelpers" do
+  include ServiceHoursHelper
+
+  describe "testtest" do
+    it "should equal Jason" do
+      helper.test.should eql("Jason")
+    end
+  end
+end
